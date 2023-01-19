eca6a3a1145e154ea9ade6e5dec4571fab0f8512
Isaque Mansur de Oliveira <isaque.oliveira@artvac.local>
2023-01-19 14:45:45 -0300

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
+    '@typescript-eslint/indent': [2, 2],
+    'linebreak-style': 0,
+    'import/extensions': 0,
+    'no-underscore-dangle': 0,
+    'no-useless-constructor': 0,
+    'class-methods-use-this': 0,
+    'max-classes-per-file': 0,
+  },
+};
